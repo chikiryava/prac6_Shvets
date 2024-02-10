95e4b1cffbb03d84a96102b86923bd4a6d0c6a3b
Viktroia <vikmolotova@gmaill.com>
2024-02-10 12:24:46 +0500

    first commit

---
+++ pract6.cpp
@@ -0,0 +1,43 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <stdio.h>
+
+bool IsWord(char* word) {
+    for (int i = 0; i < strlen(word); i++) {
+        if (!isalnum(word[i]))
+            return false;
+    }
+    return true;
+}
+
+void ShowOddWords(char* string)
+{
+    int wordNumber = 0;
+    char* words = strtok(string, " ");
+    while (words != NULL)                     
+    {
+        if (IsWord(words)) {              
+            wordNumber += 1;  
+            if (wordNumber % 2 != 0) {
+                std::cout << words << "\n";
+            }
+        }       
+        words = strtok(NULL, " ");             
+    }
+}
+void CreateString(char* string) {
+    std::cout << "Введите строку\n";
+    gets_s(string, 99);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    char str[100];
+    CreateString(str);
+    ShowOddWords(str);
+
+}
+
+
